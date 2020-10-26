7ad7cab08f0636da903f451a0579887c5f515aab
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-26 17:52:30 +0900

    first commit

---
+++ Rechap02/Circle3.java
@@ -0,0 +1,18 @@
+//なぜかエラーがでる。ファイルの先頭にエラーが表示されているが、コメントアウトなしでも変わらず。。。
+//最初からやったらできた！！！　　　なぜできたかは不明
+
+
+import java.util.Scanner;
+
+class Circle3 {
+	public static void main(String[] args) {
+		final double PI = 3.1416;		//円周率
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("半径：");
+		double r = stdIn.nextDouble();	//半径
+		
+		System.out.println("円周率の長さは" + 2 * PI * r + "です。");
+		System.out.println("面積は" + PI * r * r + "です。");
+	}
+}
