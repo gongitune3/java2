e94923a707212dd1bdc381cc51bddbd157f05a3a
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-27 15:20:58 +0900

    add

---
+++ Rechap03/Pos1.java
@@ -0,0 +1,15 @@
+//list 3-1 読み込んだ整数値は正か？
+
+import java.util.Scanner;
+
+class Pos1 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("整数値");
+        int n = stdIn.nextInt();
+
+        if (n > 0)
+            System.out.println("その値は正です。");
+    }
+}
