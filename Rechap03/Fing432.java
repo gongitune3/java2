886db8dfa780662536a205eafc701620450ebdf7
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-29 12:20:01 +0900

    finish

--- Rechap03/Fing432.java
+++ Rechap03/Fing432.java
@@ -9,10 +9,19 @@ class Fing432 {
         System.out.print("手を選んでください（０…グー／１…チョキ／２…パー）：");
         int hand = stdIn.nextInt();
 
-        if (hand == 0)
-            System.out.println("グー");
-        if (hand == 1)
-            System.out.println("チョキ");
+        // if (hand == 0)
+        //     System.out.println("グー");
+        // else if (hand == 1)
+        //     System.out.println("チョキ");
+        // else if (hand == 2)
+        //     System.out.println("パー");    if分だとスマートじゃない
+
+        switch (hand) {
+            case 0 : System.out.println("グー");    break;
+            case 1 : System.out.println("チョキ");  break;
+            case 2 : System.out.println("パー");    break;
+            
+        }
 
     }
 }
