e42043002f2044560d14caa778da6d5a09ac4e31
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-29 13:59:23 +0900

    finish

--- Rechap04/Kazu234.java
+++ Rechap04/Kazu234.java
@@ -1,5 +1,29 @@
 //list 4-3 数あてゲーム
 
+import java.util.Random;
+import java.util.Scanner;
+
 class Kazu234 {
-    
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner stdIn = new Scanner(System.in);
+
+        int no = rand.nextInt(100); //当てるべき数、０〜９９の乱数を生成
+
+        System.out.println("数当てゲーム開始！！");
+        System.out.println("０〜９９の数を当ててください。");
+
+        int x;
+        do {
+            System.out.print("ランダムに数が生成されました、いくつでしょうか。");
+            x = stdIn.nextInt();
+
+            if (x > no)
+                System.out.println("もっとアンダー");
+            else if (x < no)
+                System.out.println("もっとオーバー");
+        } while (x != no);
+
+        System.out.println("正解です。");
+    }
 }
