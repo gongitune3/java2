f2f33a7aecfed5be86de95030ed548cc3a6beb97
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-29 10:38:31 +0900

    add

---
+++ Rechap03/Digno.java
@@ -0,0 +1,19 @@
+//list 3-7　テスト
+
+import java.util.Scanner;
+
+class Digno {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("整数値：");
+        int n = stdIn.nextInt();
+
+        if (n == 0)
+            System.out.println("ゼロです。");
+        else if (n >= -9 && n <= 9)
+            System.out.println("１桁です。");
+        else
+            System.out.println("２桁以上です。");
+    }
+}
