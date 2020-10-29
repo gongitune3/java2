a08434737d497d319de4f05e11f9fb7a045cd7eb
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-29 13:49:48 +0900

    add

--- Rechap04/Sea576839.java
+++ Rechap04/Sea576839.java
@@ -1,7 +1,28 @@
 //list 4-1
 
-
+import java.util.Scanner;
 
 class Sea576839 {
-    
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int retry;
+
+        do {
+            System.out.print("季節を求めます。\n何月ですか？");
+            int month = stdIn.nextInt();
+
+            if (month >= 3 && month <= 5)
+                System.out.println("それは春です。");
+            else if (month >= 6 && month <= 8)
+                System.out.println("それは夏です。");
+            else if (month >= 9 && month <= 11)
+                System.out.println("それは秋です。");
+            else if (month == 12 || month == 1 || month ==2)
+                System.out.println("それは冬です。");
+
+            System.out.print("もう一度？　１…YES／２…NO");
+            retry = stdIn.nextInt();
+        } while (retry ==  1);
+    }
 }
