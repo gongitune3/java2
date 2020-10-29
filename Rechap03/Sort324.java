047ce9e624fe0fc20497196d7b3ab3787e181594
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-29 11:38:44 +0900

    finish

--- Rechap03/Sort324.java
+++ Rechap03/Sort324.java
@@ -1,5 +1,22 @@
 //list 3-14 二つに変数を昇順にソート、小さい順番
 
+import java.util.Scanner;
+
 class Sort324 {
-    
+    public static void main(String[] args) {
+        Scanner stdIn =  new Scanner(System.in);
+
+        System.out.print("変数a：");    int a = stdIn.nextInt();
+        System.out.print("変数b：");    int b = stdIn.nextInt();
+
+        if (a > b) {
+            int t = a;
+            a = b;
+            b = t;
+        }
+
+        System.out.println("昇順にソートしました。");
+        System.out.println("変数aは" + a + "デス。");
+        System.out.println("変数bは" + b + "デス。");
+    }
 }
