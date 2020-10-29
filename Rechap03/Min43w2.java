7e27543ed1f7a6b7c96250b588ffe6387576fe57
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-29 10:53:27 +0900

    add

---
+++ Rechap03/Min43w2.java
@@ -0,0 +1,15 @@
+//list 3-11 条件演算子
+
+import java.util.Scanner;
+
+class Min43w2 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("整数a：");    int a = stdIn.nextInt();
+        System.out.print("整数b：");    int b = stdIn.nextInt();
+
+        int min = a < b ? a :b;
+        System.out.println("小さいのは" + min + "です。");
+    }
+}
