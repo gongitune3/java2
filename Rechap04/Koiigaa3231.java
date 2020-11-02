5f7ab4a42ec5196ae4d061c9a8ac556f220ffb7f
gongitune3 <rtdmwrtdmw@gmail.com>
2020-11-02 15:36:05 +0900

    list 4-#

---
+++ Rechap04/Koiigaa3231.java
@@ -0,0 +1,12 @@
+//読み込んだ整数グループを加算(整数５個×１０グループ)
+
+import java.util.Scanner;
+
+class Koiigaa3231 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("整数を加算します。");
+        int total = 0; //全グループの合計
+    }
+}
