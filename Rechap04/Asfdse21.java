d823fd708da8bd9d70a2a51907143629e811638f
gongitune3 <rtdmwrtdmw@gmail.com>
2020-11-03 23:07:35 +0900

    add

--- Rechap04/Asfdse21.java
+++ Rechap04/Asfdse21.java
@@ -1,5 +1,13 @@
 //トライアンド
 
+import java.util.Scanner;
+
 class Asfdse21 {
-    
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("整数で加算しますか。");
+        System.out.println("何個加算しますか：");
+        int n = stdIn.nextInt(); //加算する個数。
+    }
 }
